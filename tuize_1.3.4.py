579560edeb2ad1c0c6600540b4181fada93dc8e1
Luis Daniel Herrera Aguirre <herreraluis08@outlook.com>
2019-10-26 22:28:40 -0600

    Add files via upload

---
+++ tuize_1.3.4.py
@@ -0,0 +1,71 @@
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+import cv2, time
+
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+while True:
+    video=cv2.VideoCapture(0)
+    check, frame = video.read()
+    cv2.waitKey(0)
+    video.release()
+
+    # cargar imagen, convertir a escala de grises y aplicarle filtro para reducir artefactos
+    image = frame#cv2.imread(frame)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.dilate(edged, None, iterations=10)
+    edged = cv2.erode(edged, None, iterations=10)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+    	cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    (cnts, _) = contours.sort_contours(cnts)
+    pixelsPerMetric = None
+    for c in cnts:
+    	if cv2.contourArea(c) < 100:
+    		continue
+    	orig = image.copy()
+    	box = cv2.minAreaRect(c)
+    	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    	box = np.array(box, dtype="int")
+    	box = perspective.order_points(box)
+    	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+    	for (x, y) in box:
+    		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+    	(tl, tr, br, bl) = box
+    	(tltrX, tltrY) = midpoint(tl, tr)
+    	(blbrX, blbrY) = midpoint(bl, br)
+    	(tlblX, tlblY) = midpoint(tl, bl)
+    	(trbrX, trbrY) = midpoint(tr, br)
+    	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+    	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+    	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+    	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+    	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+    		(255, 0, 255), 2)
+    	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+    		(255, 0, 255), 2)
+    	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+    	uni= raw_input("Desea hacer sus medidas en Milimetros[mm] o Pulgadas [in]")
+    	if pixelsPerMetric is None:
+    		if uni == "mm":
+    			pixelsPerMetric = dB / 24.257
+    		if uni == "in":
+    			pixelsPerMetric = dB / .955
+    	dimA = dA / pixelsPerMetric
+    	dimB = dB / pixelsPerMetric
+    	if uni == 'mm':
+    		cv2.putText(orig, "{:.1f}mm".format(dimB), (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+    		cv2.putText(orig, "{:.1f}mm".format(dimA), (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+    	if uni== "in":
+    		cv2.putText(orig, "{:.1f}in".format(dimB), (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+    		cv2.putText(orig, "{:.1f}in".format(dimA), (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+    	cv2.imshow("Image", orig)
+    	cv2.waitKey(0)
